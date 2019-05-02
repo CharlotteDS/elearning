c3856040c8a75d40fb69c9e7e53de9acb084a97d
CharlotteDS <charlotte.sacmar@gmail.com>
2019-05-02 11:40:36 +0800

    Added comment to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /elearning/tmp/pids/server.pid
 
+# This is a change
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
